5b6eb483bc6262914259a2810be1408939be4368
Juan Manuel Restrepo Galarcio <94997816+manuelRestrep0@users.noreply.github.com>
2023-05-10 12:14:11 -0500

    Update PlazoletamicroserviceApplicationTests.java

--- src/test/java/com/pragma/plazoletamicroservice/PlazoletamicroserviceApplicationTests.java
+++ src/test/java/com/pragma/plazoletamicroservice/PlazoletamicroserviceApplicationTests.java
@@ -6,8 +6,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class PlazoletamicroserviceApplicationTests {
 
-    @Test
+    /*@Test
     void contextLoads() {
     }
+
+     */
 
 }
